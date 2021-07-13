9ff327400a9273526a53642d0927df89634dfc8f
Wagner Luis Tavares De Castro <wagner.castro@cwi.com.br>
2021-07-13 17:16:15 -0300

    Initial commit

---
+++ src/main/java/br/com/cwi/treinamentorest/TreinamentoRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.cwi.treinamentorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cwi/treinamentorest/TreinamentoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cwi.treinamentorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
