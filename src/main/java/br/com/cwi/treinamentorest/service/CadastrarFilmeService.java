51b4be90ade4ca25934c64d85fcd3a8da4582e32
Wagner <wagner.ltc@gmail.com>
2021-07-16 17:08:38 -0300

    Relacionando Filme com Atores.

--- src/main/java/br/com/cwi/treinamentorest/service/CadastrarFilmeService.java
+++ src/main/java/br/com/cwi/treinamentorest/service/CadastrarFilmeService.java
@@ -1,13 +1,16 @@
 package br.com.cwi.treinamentorest.service;
 
+import br.com.cwi.treinamentorest.domain.Ator;
 import br.com.cwi.treinamentorest.domain.Diretor;
 import br.com.cwi.treinamentorest.domain.Filme;
+import br.com.cwi.treinamentorest.repository.AtorRepository;
 import br.com.cwi.treinamentorest.repository.DiretorRepository;
 import br.com.cwi.treinamentorest.repository.FilmeRepository;
 import br.com.cwi.treinamentorest.request.CadastrarFilmeRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,11 +22,16 @@ public class CadastrarFilmeService {
     @Autowired
     private DiretorRepository diretorRepository;
 
+    @Autowired
+    private AtorRepository atorRepository;
+
     public void cadastrarFilme(CadastrarFilmeRequest request) {
         final Optional<Diretor> diretor = diretorRepository.findById(request.getIdDiretor());
+        final List<Ator> atores = atorRepository.findByIdIn(request.getIdsAtores());
         filmeRepository.save( Filme.builder()
                 .titulo(request.getTitulo())
                 .diretor(diretor.orElse(null))
+                .atores(atores)
                 .build());
     }
 }
--- src/main/java/br/com/cwi/treinamentorest/service/ModificarFilmeService.java
+++ src/main/java/br/com/cwi/treinamentorest/service/ModificarFilmeService.java
@@ -1,13 +1,16 @@
 package br.com.cwi.treinamentorest.service;
 
+import br.com.cwi.treinamentorest.domain.Ator;
 import br.com.cwi.treinamentorest.domain.Diretor;
 import br.com.cwi.treinamentorest.domain.Filme;
+import br.com.cwi.treinamentorest.repository.AtorRepository;
 import br.com.cwi.treinamentorest.repository.DiretorRepository;
 import br.com.cwi.treinamentorest.repository.FilmeRepository;
 import br.com.cwi.treinamentorest.request.ModificarFilmeRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,13 +22,18 @@ public class ModificarFilmeService {
     @Autowired
     private DiretorRepository diretorRepository;
 
+    @Autowired
+    private AtorRepository atorRepository;
+
     public void modificarFilme(Long id, ModificarFilmeRequest request) {
         Optional<Diretor> diretor = diretorRepository.findById(request.getIdDiretor());
+        List<Ator> atores = atorRepository.findByIdIn(request.getIdsAtores());
         filmeRepository.save(
                 Filme.builder()
                         .id(id)
                         .titulo(request.getTitulo())
                         .diretor(diretor.orElse(null))
+                        .atores(atores)
                         .build());
     }
 }
