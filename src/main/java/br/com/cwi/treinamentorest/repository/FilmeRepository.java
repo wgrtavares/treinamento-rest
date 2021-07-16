e686feef34d40ea3edf3270b21b3ce675a7595ec
Wagner <wagner.ltc@gmail.com>
2021-07-16 17:31:57 -0300

    Customização de JPARepository com keywords em métodos.

--- src/main/java/br/com/cwi/treinamentorest/repository/FilmeRepository.java
+++ src/main/java/br/com/cwi/treinamentorest/repository/FilmeRepository.java
@@ -4,6 +4,36 @@ import br.com.cwi.treinamentorest.domain.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
+
+    /**
+     * Pesquisando filmes por título.
+     * @param titulo
+     * @return
+     */
+    Filme findByTitulo(String titulo);
+
+    /**
+     * Pequisando filmes por id do Diretor.
+     * @param id
+     * @return
+     */
+    List<Filme> findByDiretorId(Long id);
+
+    /**
+     * Contando filmes por id do Diretor.
+     * @param id
+     * @return
+     */
+    Long countByDiretorId(Long id);
+
+    /**
+     * Verifica se existe filme com o diretor.id.
+     * @param id
+     * @return
+     */
+    Boolean existsByDiretorId(Long id);
 }
