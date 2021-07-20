4d45c7c1669cbb9469d85c1656a7f760c7bafc39
Wagner <wagner.ltc@gmail.com>
2021-07-20 10:47:06 -0300

    Incluindo na aplicação do método run para teste rápido de services.

--- src/main/java/br/com/cwi/treinamentorest/TreinamentoRestApplication.java
+++ src/main/java/br/com/cwi/treinamentorest/TreinamentoRestApplication.java
@@ -1,15 +1,20 @@
 package br.com.cwi.treinamentorest;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class TreinamentoRestApplication {
+public class TreinamentoRestApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TreinamentoRestApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
